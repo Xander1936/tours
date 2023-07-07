4cb4ab76b4fe1cc5e6c5b4da908baae682d00267
Alexandre Massoda <alexandrehervemassodamambeleck@gmail.com>
2023-07-07 18:43:23 +0100

    Update of App.js with comments

--- src/App.js
+++ src/App.js
@@ -1,59 +1,71 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
+
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 
+
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [tours, setTours] = useState([])
+const [loading, setLoading] = useState(false);
+const [tours, setTours] = useState([]);
 
-  const removeTour = (id) => {
-    const newTours = tours.filter((tour) => tour.id !== id)
-    setTours(newTours)
-  }
+// Remove Tour Function
+const removeTour = (id) => {
+  const newTours = tours.filter((tour) => tour.id !== id);
+  setTours(newTours);
 
-  const fetchTours = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(url);
-      const tours = await response.json();
-      setLoading(false);
-      setTours(tours)
-    } catch (error) {
-      setLoading(false)
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    fetchTours()
-  }, []);
+}
 
-  if (loading) {
-    return (
-      <main>
-        <Loading />
-      </main>
-    );
+// Fetching the Data from the API
+const fetchTours = async () => {
+  setLoading(true);
+  try {
+    const response = await fetch(url);
+    const tours = await response.json();
+    setLoading(false);
+    setTours(tours);
+  } catch(error) {
+    setLoading(false);
+    console.log(error);
   }
-  if (tours.length === 0) {
-    return(
-      <main>
-        <div className='title'>
-          <h2>no tours left</h2>
-          <button className='btn' onClick={() => fetchTours()}>
-            refresh
-          </button>
-        </div>
-      </main>
-    )
-  }
+  console.log(tours);
+};
+
+useEffect(() => {
+  fetchTours();
+},[]);
+
+if (loading) {
   return (
     <main>
-      <Tours tours={tours} removeTour={removeTour} />
+      <Loading />
     </main>
   );
 }
+if (tours.length === 0) {
+  return (
+    <main>
+      <div className='title'>
+        <h2>no tours left.</h2>
+        {/* Refresh the page and loads new page's tours */}
+        <button className='btn' onClick={() => fetchTours()} >
+          Refresh
+        </button>
+      </div>
+    </main>
+  )
+}
 
-export default App
+
+return (
+  <main>
+    {/* Pass tours as a prop
+      on the Tours component */}
+    <Tours tours={tours} removeTour={removeTour} />
+  </main>
+)
+}
+
+export default App;
